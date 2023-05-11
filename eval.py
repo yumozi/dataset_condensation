Add normalization fix to torchattacks

eval.py
@@ -5,7 +5,8 @@ import torch
 import torch.nn as nn
 import pdb
 import sys
-from utils import get_dataset, epoch, epoch_alt, get_network, TensorDataset, Attack, ParamAttack
+import numpy as np
+from utils import get_dataset, epoch, epoch_alt, get_network, TensorDataset, Attack, ParamAttack, NormalizeByChannelMeanStd
 import torchattacks
 
 
@@ -15,7 +16,7 @@ def main():
     parser.add_argument('--model', type=str, default='ConvNet', help='model')
     parser.add_argument('--real_dataset', type=str, default='CIFAR10', help='dataset')
     parser.add_argument('--real_data_path', type=str, default='data', help='real dataset path')
-
+    parser.add_argument('--syn_dataset', type=str, default='CIFAR10', help='original dataset of synthetic data')
     parser.add_argument('--train_data_type', type=str, default='syn', help='train model with syn or real data')
     parser.add_argument('--syn_data_type', type=str, default='Attack', help='which synthetic data to use, Attack/DC/DSA')
     parser.add_argument('--save_path', type=str, default='result', help='path to save results')
@@ -30,7 +31,7 @@ def main():
     parser.add_argument('--aux_bn', type=bool, default=False, help='whether to apply auxiliary BN for training')
     parser.add_argument('--alp_embed', type=bool, default=False, help='whether to apply adversarial logit pairing for training')
     parser.add_argument('--alp_live', type=bool, default=False, help='whether to apply adversarial logit pairing for training')
-
+    parser.add_argument('--ipc', type=int, default=10, help='images per class')
     # parser.add_argument('--eval_mode', type=str, default='SS', help='eval_mode') # S: the same to training model, M: multi architectures,  W: net width, D: net depth, A: activation function, P: pooling layer, N: normalization layer,
     # parser.add_argument('--num_exp', type=int, default=5, help='the number of experiments')
     # parser.add_argument('--num_eval', type=int, default=20, help='the number of evaluating randomly initialized models')
@@ -43,7 +44,7 @@ def main():
 
     # If aux_bn is True, also load Attacked Distilled Data
     if args.aux_bn or args.alp_embed or args.alp_live:
-        args.aux_data_path = f'./result/res_Attack_CIFAR10_ConvNet_10ipc.pt'
+        args.aux_data_path = f'./result/res_Attack_{args.syn_dataset}_ConvNet_{args.ipc}ipc.pt'
         if not os.path.exists(args.aux_data_path):
             print(f'Error: Attack data not found.')
             sys.exit(1)
@@ -61,7 +62,7 @@ def main():
         print(f'Error: {args.real_data_path} not found.')
         sys.exit(1)
 
-    args.syn_data_path = f'./result/res_{args.syn_data_type}_CIFAR10_ConvNet_10ipc.pt'
+    args.syn_data_path = f'./result/res_{args.syn_data_type}_{args.syn_dataset}_ConvNet_{args.ipc}ipc.pt'
     if not os.path.exists(args.syn_data_path):
         print(f'Error: {args.syn_data_path} not found.')
         sys.exit(1)
@@ -83,30 +84,36 @@ def main():
     channel = 3
     num_classes = 10
     im_size = (32, 32)
-
-        
+    if args.syn_dataset == 'MNIST':
+        channel = 1
+        im_size = (28, 28)
+    elif args.syn_dataset == 'CIFAR100':
+        num_classes = 100
 
     # Load Real Data (only need info if using real data to train)
+    channel, im_size, num_classes, class_names, mean, std, dst_train, dst_test, testloader = get_dataset(args.real_dataset, args.real_data_path)
     if args.train_data_type == 'real':
-        channel, im_size, num_classes, class_names, mean, std, dst_train, dst_test, testloader = get_dataset(args.real_dataset, args.real_data_path)
         real_train_images = [torch.unsqueeze(dst_train[i][0], dim=0) for i in range(len(dst_train))]
         real_train_labels = [dst_train[i][1] for i in range(len(dst_train))]
         real_train_images = torch.cat(real_train_images, dim=0).to(args.device)
         real_train_labels = torch.tensor(real_train_labels, dtype=torch.long, device=args.device)
-    else:
-        dst_test = get_dataset(args.real_dataset, args.real_data_path)[7]
 
     real_test_images = [torch.unsqueeze(dst_test[i][0], dim=0) for i in range(len(dst_test))]
     real_test_labels = [dst_test[i][1] for i in range(len(dst_test))]
     real_test_images = torch.cat(real_test_images, dim=0).to(args.device)
     real_test_labels = torch.tensor(real_test_labels, dtype=torch.long, device=args.device)
 
+    normalize = NormalizeByChannelMeanStd(mean=mean, std=std)
 
-    acc_total = 0
+    acc_list = []
     for trial in range(args.trials):
 
         # Setup net
         net = get_network(args.model, channel, num_classes, im_size).to(args.device) # get a random model
+
+        # torchattack fix
+        net = nn.Sequential(normalize, net)
+
         net.train()
         net_parameters = list(net.parameters())
         if args.train_data_type == 'syn':
@@ -148,13 +155,14 @@ def main():
         # Test net
         loss, acc = epoch('test', testloader, net, optimizer_net, criterion, args, aug = False)
         print(f'Trial {trial}: acc = {acc}')
-        acc_total += acc
+        acc_list.append(acc)
 
     if args.attack_eval:
         print(f'Trained with {args.train_data_type} data ({args.syn_data_type}), tested on {args.attack_strategy} attacked {args.real_dataset} dataset.')
     else:
         print(f'Trained with {args.train_data_type} data ({args.syn_data_type}), tested on normal {args.real_dataset} dataset.')
-    print(f'Average accuracy over {args.trials} trials is {round(acc_total / args.trials, 4)}')
+
+    print(f'Average accuracy over {args.trials} trials is {round(np.mean(acc_list) * 100, 1)}±{round(np.std(acc_list) * 100, 1)}')
 
 if __name__ == '__main__':
     main()

main_CURE.py
@@ -6,7 +6,7 @@ import numpy as np
 import torch
 import torch.nn as nn
 from torchvision.utils import save_image
-from utils import get_loops, get_dataset, get_network, get_eval_pool, evaluate_synset, get_daparam, match_loss, get_time, TensorDataset, epoch, DiffAugment, ParamDiffAug, Attack, ParamAttack
+from utils import get_loops, get_dataset, get_network, get_eval_pool, evaluate_synset, get_daparam, match_loss, get_time, TensorDataset, epoch, DiffAugment, ParamDiffAug, Attack, ParamAttack, NormalizeByChannelMeanStd
 
 
 def main():
@@ -19,7 +19,7 @@ def main():
     parser.add_argument('--eval_mode', type=str, default='S', help='eval_mode') # S: the same to training model, M: multi architectures,  W: net width, D: net depth, A: activation function, P: pooling layer, N: normalization layer,
     parser.add_argument('--attack-eval', action='store_true')
     parser.add_argument('--num_exp', type=int, default=1, help='the number of experiments')
-    parser.add_argument('--num_eval', type=int, default=50, help='the number of evaluating randomly initialized models')
+    parser.add_argument('--num_eval', type=int, default=20, help='the number of evaluating randomly initialized models')
     parser.add_argument('--epoch_eval_train', type=int, default=300, help='epochs to train a model with synthetic data')
     parser.add_argument('--Iteration', type=int, default=1000, help='training iterations')
     parser.add_argument('--lr_img', type=float, default=0.1, help='learning rate for updating synthetic images')
@@ -55,6 +55,7 @@ def main():
     print('eval_it_pool: ', eval_it_pool)
     channel, im_size, num_classes, class_names, mean, std, dst_train, dst_test, testloader = get_dataset(args.dataset, args.data_path)
     model_eval_pool = get_eval_pool(args.eval_mode, args.model, args.model)
+    normalize = NormalizeByChannelMeanStd(mean=mean, std=std)
 
 
     accs_all_exps = dict() # record performances of all experiments
@@ -142,6 +143,10 @@ def main():
                     accs = []
                     for it_eval in range(args.num_eval):
                         net_eval = get_network(model_eval, channel, num_classes, im_size).to(args.device) # get a random model
+
+                        # torchattack fix
+                        net_eval = nn.Sequential(normalize, net_eval)
+
                         image_syn_eval, label_syn_eval = copy.deepcopy(image_syn.detach()), copy.deepcopy(label_syn.detach()) # avoid any unaware modification
                         _, acc_train, acc_test = evaluate_synset(it_eval, net_eval, image_syn_eval, label_syn_eval, testloader, args)
                         accs.append(acc_test)
@@ -162,6 +167,10 @@ def main():
 
             ''' Train synthetic data '''
             net = get_network(args.model, channel, num_classes, im_size).to(args.device) # get a random model
+
+            # torchattack fix
+            net = nn.Sequential(normalize, net)
+
             net.train()
             net_parameters = list(net.parameters())
             optimizer_net = torch.optim.SGD(net.parameters(), lr=args.lr_net)  # optimizer_img for synthetic data
@@ -204,7 +213,7 @@ def main():
                         img_real = DiffAugment(img_real, args.dsa_strategy, seed=seed, param=args.dsa_param)
                         img_syn = DiffAugment(img_syn, args.dsa_strategy, seed=seed, param=args.dsa_param)
 
-                    if args.method == 'Attack':
+                    if args.attack:
                         seed = int(time.time() * 1000) % 100000
                         # We only want to attack the real dataset
                         img_real = Attack(img_real, lab_real, net, args.attack_strategy, seed=seed, param=args.attack_param)

utils.py
@@ -18,7 +18,7 @@ def get_dataset(dataset, data_path):
         num_classes = 10
         mean = [0.1307]
         std = [0.3081]
-        transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize(mean=mean, std=std)])
+        transform = transforms.Compose([transforms.ToTensor()])
         dst_train = datasets.MNIST(data_path, train=True, download=True, transform=transform) # no augmentation
         dst_test = datasets.MNIST(data_path, train=False, download=True, transform=transform)
         class_names = [str(c) for c in range(num_classes)]
@@ -29,7 +29,7 @@ def get_dataset(dataset, data_path):
         num_classes = 10
         mean = [0.2861]
         std = [0.3530]
-        transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize(mean=mean, std=std)])
+        transform = transforms.Compose([transforms.ToTensor()])
         dst_train = datasets.FashionMNIST(data_path, train=True, download=True, transform=transform) # no augmentation
         dst_test = datasets.FashionMNIST(data_path, train=False, download=True, transform=transform)
         class_names = dst_train.classes
@@ -40,7 +40,7 @@ def get_dataset(dataset, data_path):
         num_classes = 10
         mean = [0.4377, 0.4438, 0.4728]
         std = [0.1980, 0.2010, 0.1970]
-        transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize(mean=mean, std=std)])
+        transform = transforms.Compose([transforms.ToTensor()])
         dst_train = datasets.SVHN(data_path, split='train', download=True, transform=transform)  # no augmentation
         dst_test = datasets.SVHN(data_path, split='test', download=True, transform=transform)
         class_names = [str(c) for c in range(num_classes)]
@@ -51,7 +51,7 @@ def get_dataset(dataset, data_path):
         num_classes = 10
         mean = [0.4914, 0.4822, 0.4465]
         std = [0.2023, 0.1994, 0.2010]
-        transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize(mean=mean, std=std)])
+        transform = transforms.Compose([transforms.ToTensor()])
         dst_train = datasets.CIFAR10(data_path, train=True, download=True, transform=transform) # no augmentation
         dst_test = datasets.CIFAR10(data_path, train=False, download=True, transform=transform)
         class_names = dst_train.classes
@@ -62,7 +62,7 @@ def get_dataset(dataset, data_path):
         num_classes = 100
         mean = [0.5071, 0.4866, 0.4409]
         std = [0.2673, 0.2564, 0.2762]
-        transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize(mean=mean, std=std)])
+        transform = transforms.Compose([transforms.ToTensor()])
         dst_train = datasets.CIFAR100(data_path, train=True, download=True, transform=transform) # no augmentation
         dst_test = datasets.CIFAR100(data_path, train=False, download=True, transform=transform)
         class_names = dst_train.classes
@@ -81,8 +81,8 @@ def get_dataset(dataset, data_path):
         labels_train = data['labels_train']
         images_train = images_train.detach().float() / 255.0
         labels_train = labels_train.detach()
-        for c in range(channel):
-            images_train[:,c] = (images_train[:,c] - mean[c])/std[c]
+        # for c in range(channel):
+        #     images_train[:,c] = (images_train[:,c] - mean[c])/std[c]
         dst_train = TensorDataset(images_train, labels_train)  # no augmentation
 
         images_val = data['images_val']
@@ -719,10 +719,30 @@ def rand_cutout(x, param):
 
 class ParamAttack:
     def __init__(self):
-        # self.eps = 8/255
-        self.eps = 8/255
-        self.alpha = 1/255
+        # MNIST
+        # self.eps = 0.3
+        # self.alpha = 0.1
+        # self.step = 10
+        self.eps = 0.1
+        self.alpha = 0.03
         self.step = 10
+        # self.eps = 1
+        # self.alpha = 0.3
+        # self.step = 10
+
+        # CIFAR10
+        # self.eps = 8/255
+        # self.alpha = 2/255
+        # self.step = 10
+        # self.eps = 8/255
+        # self.alpha = 0.5/255
+        # self.step = 10
+        # self.eps = 0.2
+        # self.alpha = 0.05
+        # self.step = 10
+
+      
+
 
 def set_seed_Attack(param):
     if param.latestseed == -1:
@@ -765,10 +785,16 @@ def auto_attack(x, y, net, param):
     x = attack(x, y)
     return x
 
+def pgdl2(x, y, net, param):
+    attack = torchattacks.PGDL2(net, eps=param.eps, alpha=param.alpha, steps=param.step)
+    x = attack(x, y)
+    return x
+
 ATTACK_FNS = {
     'pgd' : pgd,
     'fgsm' : fgsm,
     'auto_attack' : auto_attack,
+    'pgdl2' : pgdl2,
 }
 
 
@@ -780,3 +806,31 @@ AUGMENT_FNS = {
     'scale': [rand_scale],
     'rotate': [rand_rotate],
 }
+
+def normalize_fn(tensor, mean, std):
+    """Differentiable version of torchvision.functional.normalize"""
+    # here we assume the color channel is in at dim=1
+    mean = mean[None, :, None, None]
+    std = std[None, :, None, None]
+    return tensor.sub(mean).div(std)
+
+
+class NormalizeByChannelMeanStd(nn.Module):
+    def __init__(self, mean, std, others=None):
+        super(NormalizeByChannelMeanStd, self).__init__()
+        if not isinstance(mean, torch.Tensor):
+            mean = torch.tensor(mean)
+        if not isinstance(std, torch.Tensor):
+            std = torch.tensor(std)
+        self.others = others  # other transformation
+        self.register_buffer("mean", mean)
+        self.register_buffer("std", std)
+
+    def forward(self, tensor):
+        normalized = normalize_fn(tensor, self.mean, self.std)
+        if self.others is not None:
+            normalized = self.others(normalized)
+        return normalized
+
+    def extra_repr(self):
+        return 'mean={}, std={}'.format(self.mean, self.std)